2feae71e16a1ae9d1af11c0a5b50649ab5a68d39
iyinoluwAA <iyinoluwa150@gmail.com>
2024-03-05 10:35:10 +0100

    formula with variable

---
+++ script.js
@@ -0,0 +1,51 @@
+// Area of a circle
+// var pi = 22/7
+// var radius = Number(prompt("Enter the radius of the Circle"))
+// var area_of_a_circle = pi * radius * radius;
+// console.log("The area of the Circle is: " + area_of_a_circle);
+
+// Area of A Square
+// var length = Number(prompt('enter the length of the Square'))
+// var area_of_a_length = length * length;
+// console.log("The area of the Square is: " + area_of_a_length);
+
+// Area of A Quadrilateral
+// var base = Number(prompt('Enter the base of the Quadilateral') 
+// var height = Number(prompt('Enter the height of the Quadilateral')) 
+// var area_of_a_quadrilateral = base * height;
+// console.log("The area of the Quadrilateral is: " + area_of_a_quadrilateral);
+
+// Area of A Triangle
+
+// var half = 0.5
+// var base = Number(prompt('Enter the base of Triangle'))
+// var height = Number(prompt('Enter the height of the Triangle'))
+// var area_of_a_triangle = half * base * height;
+// console.log( "The area of the Triangle is: " + area_of_a_triangle);
+
+// Area Of A rhombus
+// var half = 0.5
+// var diagonal1 = Number(prompt('Enter the diagonal1 of the Rhombus'))
+// var diagonal2 = Number(prompt('Enter the diagonal2 of the Rhombus'))
+// var area_of_a_rhombus = half * diagonal1 * diagonal2
+// console.log( "The area of the Rhombus is: " + area_of_a_rhombus);
+
+// Area of body mass index
+    // var weight_kg = Number(prompt('Enter the weight in kilograms'))
+    // var height_m = Number(prompt('Enter the height in meter²'))
+    // var area_of_body_mass_index = weight_kg / (height_m ** 2)
+    // console.log( "The area of the Body mass index is: " + area_of_body_mass_index);
+
+    // Area of a Trapezium
+    // var half = 0.5
+    //  var base1 = Number(prompt('Enter the base1 of the Trapezium'))
+    //  var base2 = Number(prompt('Enter the base2 of the Trapezium'))
+    //  var height = Number(prompt(' Enter the height of the Trapezium'))
+    //  var area_of_a_trapezium = half * Number(base1 + base2) * height
+    // console.log( "The area of the Trapezium is: " + area_of_a_trapezium);
+    
+    // Area of a parellelogram
+        // var base = Number(prompt('Enter the base of parallelogram '))
+        // var height = Number(prompt('Enter the height of the parallelogram '))
+        // var area_of_a_parallelogram =  base * height;
+        // console.log( "The area of the Parallelogram is: " + area_of_a_parallelogram );
